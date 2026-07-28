bb5edb7e90c18ade3b5683768bde5ce7dc534eaf
heba taha <amltaha866@yahoo.om>
2026-07-28 14:27:19 +0300

    add Popular Catagories

--- src/app/app.ts
+++ src/app/app.ts
@@ -3,10 +3,11 @@ import { RouterOutlet } from '@angular/router';
 import { Header } from './core/layout/header/header';
 import { Banner } from "./components/banner/banner";
 import { Footer } from './core/layout/footer/footer';
+import { PopularCategories } from './components/popular-categories/popular-categories';
 
 @Component({
   selector: 'app-root',
-  imports: [RouterOutlet, Header, Banner,Footer],
+  imports: [RouterOutlet, Header, Banner,Footer,PopularCategories],
   templateUrl: './app.html',
   styleUrl: './app.scss'
 })
---
+++ src/app/components/popular-categories/popular-categories.ts
@@ -0,0 +1,13 @@
+import { Component } from '@angular/core';
+import { Card } from './card/card';
+
+@Component({
+  selector: 'app-popular-categories',
+  imports: [Card],
+  templateUrl: './popular-categories.html',
+  styleUrl: './popular-categories.scss',
+})
+export class PopularCategories {
+
+
+}
